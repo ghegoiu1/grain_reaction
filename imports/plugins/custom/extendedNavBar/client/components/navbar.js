8675a1453664a2935ca02628998e8bc2c0d47418
ghegoiu1 <ghegoiustefan@gmail.com>
2017-12-23 03:42:39 +0200

    navbar link added

---
+++ imports/plugins/custom/extendedNavBar/client/components/navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { replaceComponent } from "/imports/plugins/core/components/lib";
+import { getRawComponent } from "@reactioncommerce/reaction-components";
+import { Reaction } from "/client/api";
+import { Logger } from "/client/api";
+
+const NavBar = getRawComponent("NavBar");
+
+class CustomNavBar extends NavBar {
+	renderCustomMenu() {
+		// Logger.info("Goes in here");
+		const routes = [
+			{
+				"path": "aboutUs",
+				"label": "About Us"
+			}];
+
+		const routesList = routes.map(function(route) {
+			return <a href={Reaction.Router.pathFor(route.path)}> {route.label} </a>; 
+		});
+
+		return <div> {routesList} </div>;
+	}
+
+	render() {
+		return (
+			<div className="rui navbar">
+		        {this.props.visibility.hamburger && this.renderHamburgerButton()}
+		        {this.props.visibility.brand && this.renderBrand()}
+		        {this.props.visibility.tags && this.renderTagNav()}
+		        {this.renderCustomMenu()} 
+		        {this.props.visibility.search && this.renderSearchButton()}
+		        {this.props.visibility.notifications && this.renderNotificationIcon()}
+		        {this.props.visibility.languages && this.renderLanguage()}
+		        {this.props.visibility.currency && this.renderCurrency()}
+		        {this.props.visibility.mainDropdown && this.renderMainDropdown()}
+		        {this.props.visibility.cartContainer && this.renderCartContainerAndPanel()}
+	      </div>
+			);
+	}
+}
+
+replaceComponent("NavBar", CustomNavBar);
+
+export default CustomNavBar;
---
+++ imports/plugins/custom/extendedNavBar/register.js
@@ -0,0 +1,7 @@
+import { Reaction } from "/server/api";
+
+Reaction.registerPackage({
+  label: "Custom UI Navbar",
+  name: "custom-reaction-ui-navbar",
+  autoEnable: true
+});
